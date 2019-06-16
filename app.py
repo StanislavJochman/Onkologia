e480a987ee87ee8e98deb733cefe67fcb26a3dff
StanislavJochman <s.jochman@gmail.com>
2019-06-16 18:54:48 +0200

    commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-app = Flask(__name__, static_url_path = "/img", static_folder = "img",static_url_path = "/css", static_folder = "css")
+app = Flask(__name__, static_url_path = "/img", static_folder = "img", static_folder = "css")
 
 
 @app.route("/")
