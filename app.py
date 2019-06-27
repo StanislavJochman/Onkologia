ea24a26488f2f52a03b186b1a355c9334b537e43
StanislavJochman <s.jochman@gmail.com>
2019-06-27 23:16:18 +0200

    commit

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask import render_template
-app = Flask(__name__, static_url_path = "/page", static_folder = "page")
-
-
-@app.route("/")
-def hello():
-    return render_template('index.html')
