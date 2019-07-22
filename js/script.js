cb2185942c5882b0d0fb847283482b9a22b58038
StanislavJochman <s.jochman@gmail.com>
2019-07-22 14:24:03 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -41,4 +41,5 @@ function onSignIn(googleUser) {
   // The ID token you need to pass to your backend:
   var id_token = googleUser.getAuthResponse().id_token;
   console.log("ID Token: " + id_token);
+  alert("loggedin")
 }
