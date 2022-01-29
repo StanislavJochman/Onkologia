4273144621b71a3a63b66e50ca2d7e848f2584f3
StanislavJochman <s.jochman@gmail.com>
2022-01-29 16:33:39 +0100

    update konferencii

--- js/time.js
+++ js/time.js
@@ -4,20 +4,20 @@ const second = 1000,
       day = hour * 24;
 
 let countDown1 = new Date('Sep 30, 2021 09:00:00').getTime();
-let countDown2 = new Date('Feb 11, 2022 09:00:00').getTime();
+let countDown2 = new Date('Apr 1, 2022 09:00:00').getTime();
     x = setInterval(function() {
 
     let now = new Date().getTime();
     distance1 = countDown1 - now;
     distance2 = countDown2 - now;
 
-    document.getElementById('time1').innerText = "O " + Math.floor(distance1 / (day)) + " dní";
+    //document.getElementById('time1').innerText = "O " + Math.floor(distance1 / (day)) + " dní";
     document.getElementById('time2').innerText = "O " + Math.floor(distance2 / (day)) + " dní";
       
-    if(distance1 < 0) {
-      clearInterval(x);
-      document.getElementById("time1").innerHTML = "Prebehlo";
-    }
+    //if(distance1 < 0) {
+    //  clearInterval(x);
+    //  document.getElementById("time1").innerHTML = "Prebehlo";
+    //}
     if(distance2 < 0) {
         clearInterval(x);
         document.getElementById("time2").innerHTML = "Prebehlo";
